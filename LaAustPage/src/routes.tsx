b7445f258df31383b4bcdd5d706281801c80d4a7
Marcelo Barbosa <celo.filho1@hotmail.com>
2022-08-11 18:04:42 -0300

    Adição de pastas base

---
+++ LaAustPage/src/routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes as RoutesReact } from 'react-router-dom'
+
+export function Router() {
+  return (
+    <>
+      <BrowserRouter>
+        <RoutesReact></RoutesReact>
+      </BrowserRouter>
+    </>
+  )
+}
