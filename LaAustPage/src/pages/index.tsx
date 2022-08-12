f89a5fb1ac09aa80f765203b782af1c53b04cb43
Marcelo Barbosa <celo.filho1@hotmail.com>
2022-08-11 21:01:10 -0300

    Adição da primeira seção

--- LaAustPage/src/pages/index.tsx
+++ LaAustPage/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header'
-import imgThinFont from '../assets/imgFirstSection.svg'
+import imgThinFont from '../assets/imgThinFont.svg'
+import Button from '../components/Button'
 import './style.scss'
 export default function Home() {
   return (
@@ -8,11 +9,15 @@ export default function Home() {
       <main>
         <section className="firstSection mainContainer">
           <div className="containerMain">
-            <h1>Vamos criar para você uma marca única e memorável</h1>
+            <h1>
+              Vamos criar para você uma
+              <br /> marca única e memorável
+            </h1>
             <img
               src={imgThinFont}
               alt="Perfeita para os seus clientes ideais"
             />
+            <Button text="Quero Criar Minha Marca!" />
           </div>
         </section>
       </main>
